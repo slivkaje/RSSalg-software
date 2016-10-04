39f97b7cc341b0d096e936c43d2b1b9ba05df1d3
Jelena <slivkaje@gmail.com>
2016-10-04 19:01:17 +0200

    Copyright declaration added to source files headers

--- src/application/StartExperiment.java
+++ src/application/StartExperiment.java
@@ -1,3 +1,34 @@
+/** 	
+ * Name: StartExperiment.java
+ * 
+ * Purpose: The main class that orchestrates program execution
+ * 
+ * Author: Jelena Slivka <slivkaje AT uns DOT ac DOT rs>
+ * 
+ * Copyright: (c) 2016 Jelena Slivka <slivkaje AT uns DOT ac DOT rs>
+ * 
+ * This file is a part of RSSalg software, a flexible, highly configurable tool for experimenting 
+ * with co-training based techniques. RSSalg Software encompasses the implementation of 
+ * co-training and RSSalg, a co-training based technique that can be applied to single-view 
+ * datasets published in the paper: 
+ * 
+ * Slivka, J., Kovacevic, A. and Konjovic, Z., 2013. 
+ * Combining Co-Training with Ensemble Learning for Application on Single-View Natural 
+ * Language Datasets. Acta Polytechnica Hungarica, 10(2).
+ *   
+ * RSSalg software is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * RSSalg software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ **/
 package application;
 
 import java.awt.EventQueue;
